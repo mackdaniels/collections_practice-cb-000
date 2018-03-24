12e0f8bb409498e62317b51589c7bc34e1bec9be
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-24 03:07:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|i| i.length}
+end
+
+def swap_elements(array)
+  snd = array[1]
+  trd = array[2]
+  array[1] = trd
+  array[2] = snd
+  array
+end
+
+def reverse_array(array)
+  snd = array[0]
+  trd = array[2]
+  array[0] = trd
+  array[2] = snd
+  array
+end
+
+
+def kesha_maker(array)
+  a.each do |i|
+    i[2] = "$"
+  end
+end
